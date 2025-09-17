78fa2df25ca743f39ebf607b58f87764666bc9e7
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-18 01:15:33 +0600

    form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,19 @@
 const Form = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(e);
+  };
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select name="" id=""></select>
+      <select name="" id="">
+        {Array.from({ length: 20 }, (_, index) => index + 1).map(num => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
       <input type="text" placeholder="Item..." />
       <button>Add</button>
     </form>
@@ -10,3 +21,4 @@ const Form = () => {
 };
 
 export default Form;
+//instead of listening to onSubmit we can also listen for onClick in the button <button onClick={}>Add</button> but then the submit will only work when we click this button and then it would not work for pressing ENTER key as we know pressing ENTER will also submit the form

0dc508c14aece8b8b2ec9fc140fc7b81cc86b9b3
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-22 14:43:16 +0600

    Sorting Items

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Item from './Item';
 // const initialItems = [
 //   { id: 1, description: 'Passports', quantity: 2, packed: false },
@@ -5,10 +6,22 @@ import Item from './Item';
 // ];
 
 const PackingList = ({ items, handleDeleteItems, handleToggleItem }) => {
+  const [sortBy, setSortBy] = useState('input');
+  let sortedItems;
+  if (sortBy === 'input') sortedItems = items;
+  if (sortBy === 'description')
+    sortedItems = [...items].sort((a, b) =>
+      a.description.localeCompare(b.description)
+    );
+  if (sortBy === 'packed')
+    sortedItems = [...items].sort(
+      (a, b) => Number(a.packed) - Number(b.packed)
+    );
+
   return (
     <div className="list">
       <ul>
-        {items.map(item => (
+        {sortedItems.map(item => (
           <Item
             item={item}
             key={item.id}
@@ -17,6 +30,13 @@ const PackingList = ({ items, handleDeleteItems, handleToggleItem }) => {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+          <option value="input">Sort By Input Order</option>
+          <option value="description">Sort By Description</option>
+          <option value="packed">Sort By Packed Status</option>
+        </select>
+      </div>
     </div>
   );
 };
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,16 +1,30 @@
 const Stats = ({ items }) => {
+  if (items.length === 0) {
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🧳🧳🧳</em>
+      </footer>
+    );
+  }
+
   const numItems = items.length;
   const packedItems = items.filter(item => item.packed).length;
   const percentagePackedItems = Math.round((packedItems / numItems) * 100);
 
+  const statsText = () => {
+    if (percentagePackedItems === 100) {
+      return `You've got everything ready to goooooo.... ✈️✈️✈️`;
+    }
+    if (percentagePackedItems === 0) {
+      return `💼 You've ${numItems} items in your list and You didn't pack anything...Hurry up or you will miss the flight ⌚⌚⌚`;
+    }
+    return `💼 You've ${numItems} items in your list and you already packed ${packedItems}
+        (${percentagePackedItems})% 👀👀👀`;
+  };
+
   return (
     <footer className="stats">
-      <em>
-        {percentagePackedItems === 100
-          ? `You've got everything ready to goooooo.... ✈️✈️✈️`
-          : `💼 You've ${numItems} items in your list and you already packed ${packedItems}
-        (${percentagePackedItems})% 👀`}
-      </em>
+      <em>{statsText()}</em>
     </footer>
   );
 };
